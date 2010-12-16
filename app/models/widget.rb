72263010a83cdbb557dc2ca96d23c1e5ba8cd66c
Mark Kinkade <mark.kinkade@unsw.edu.au>
2010-12-16 12:09:10 +1100

    updated to v3

--- app/models/widget.rb
+++
@@ -1,3 +0,0 @@
-class Widget < ActiveRecord::Base
-  has_many :videos, :dependent => :destroy
-end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'a41ff9a64cc5deb93284afaf2c32f6861625199f934e6a507a93c6e3bb3cfb659dc1eb7a02dcdffa74469f50bbab0a7dfa6b6138bd799df436ca6b971589ac36';
+Tvfeed::Application.config.secret_token = 'e9bc0e85b1d672e5fb797e258c1d236669269b3c68ca944aadafb59250e7e2953b98abfe17b8263ba43fe766008f8d0d347336ccc5a6c00961b69e87db77f82f'
