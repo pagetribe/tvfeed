063d423dc18ad0b73b21a651e725f982aead1f66
Mark Kinkade <mark.kinkade@unsw.edu.au>
2011-01-19 12:32:08 +1100

    minor update to jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(function() {
-//    alert("hello");
+//$(document).ready(function() { 
+//    $('#accordion').accordion();
 //    console.log("from application.js");
-})
+//});
+//$(function() {
+////    alert("hello");
+////    console.log("from application.js");
+//})
